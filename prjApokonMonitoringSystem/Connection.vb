ef50cb3b9a9e4392aa872840ab7d806d67111098
vinxentience <canlas.reyvincent@gmail.com>
2022-08-17 07:23:48 +0800

    .

--- prjApokonMonitoringSystem/Connection.vb
+++ prjApokonMonitoringSystem/Connection.vb
@@ -1,4 +1,5 @@
-﻿Imports MySql.Data.MySqlClient
+﻿Imports CloudinaryDotNet
+Imports MySql.Data.MySqlClient
 Module Connection
     Public conn As New MySqlConnection
     Public comm As New MySqlCommand
@@ -6,18 +7,12 @@ Module Connection
     Public adapter As MySqlDataAdapter
     Public school_email As String = "apokon.monitoring@gmail.com"
     Public school_password As String = "txyuonmqgfxekmeb"
+    Public Cloudinary As Cloudinary
+    Public Const CLOUD_NAME As String = "dsffeuudt"
+    Public Const API_KEY As String = "176669418197682"
+    Public Const API_SECRET As String = "OGlji-Y32d7pWMFJRf9P45mLWC8"
     Sub connect()
 
-        'Try
-        '    With conn
-        '        .ConnectionString = "server=vmucrn65rion.ap-southeast-2.psdb.cloud;user=uni5ebv1yrc3;database=monitoring_system;port=3306;password=pscale_pw_roJcqGhDx6m_f7Fd2CnevLvMauZ6cnAL24UkXwsIURc;SslMode=VerifyFull"
-        '        If .State = ConnectionState.Open Then
-        '            conn.Close()
-        '        End If
-        '    End With
-        'Catch myerror As MySqlException
-        '    MsgBox(myerror.Message)
-        'End Try
 
         Try
             With conn
