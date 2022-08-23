b172a012717690f7907afdc8d9e5f4069f74a75a
vinxentience <canlas.reyvincent@gmail.com>
2022-08-24 00:07:13 +0800

    Revert "."
    This reverts commit 7be564c294d2c8dcb7cc6275ad91f8a2fc12f96b.

--- prjApokonMonitoringSystem/Connection.vb
+++ prjApokonMonitoringSystem/Connection.vb
@@ -1,12 +1,19 @@
 ﻿Imports CloudinaryDotNet
 Imports MySql.Data.MySqlClient
-Imports System.Net
 Module Connection
     Public conn As New MySqlConnection
     Public comm As New MySqlCommand
     Public reader As MySqlDataReader
     Public adapter As MySqlDataAdapter
+    Public school_email As String = "apokon.monitoring@gmail.com"
+    Public school_password As String = "txyuonmqgfxekmeb"
+    Public Cloudinary As Cloudinary
+    Public Const CLOUD_NAME As String = "dsffeuudt"
+    Public Const API_KEY As String = "176669418197682"
+    Public Const API_SECRET As String = "OGlji-Y32d7pWMFJRf9P45mLWC8"
     Sub connect()
+
+
         Try
             With conn
                 .ConnectionString = "server=localhost;user=root;database=monitoring_system;port=3306;password=1234;"
@@ -17,18 +24,6 @@ Module Connection
         Catch myerror As MySqlException
             MsgBox(myerror.Message)
         End Try
+
     End Sub
-
-    Public Function CheckForInternetConnection() As Boolean
-        Try
-            Using client = New WebClient()
-                Using stream = client.OpenRead("http://www.google.com")
-                    Return True
-                End Using
-            End Using
-        Catch
-            Return False
-        End Try
-    End Function
-
 End Module
