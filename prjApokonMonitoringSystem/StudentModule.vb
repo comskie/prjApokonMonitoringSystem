cd5d35b7db1e490f4c517182bc1a21940f9cbcdd
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-09-01 19:41:09 +0800

    Create StudentModule.vb

---
+++ prjApokonMonitoringSystem/StudentModule.vb
@@ -0,0 +1,17 @@
+﻿Module StudentModule
+    Function GetFieldOfStudentLogType(studentLogType As Enums.StudentLogType)
+        Select Case studentLogType
+            Case StudentLogType.TimeInAM
+                Return "timeIN_AM"
+            Case StudentLogType.TimeOutAM
+                Return "timeOUT_AM"
+            Case StudentLogType.TimeInPM
+                Return "timeIN_PM"
+            Case StudentLogType.TimeOutPM
+                Return "timeOUT_PM"
+            Case Else
+                Throw New Exception("Error log type!")
+        End Select
+    End Function
+
+End Module
